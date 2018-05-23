9b1cdb3deabe1b1f2b2a33765eb4e2f7659d1620
Igor Tonachov <igortonachov@gmail.com>
2018-05-23 11:30:18 +0300

    bidirectional relations

--- schema/schema.js
+++ schema/schema.js
@@ -4,22 +4,30 @@ const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
   GraphQLInt,
   GraphQLSchema,
 } = graphql;
 
-const  CompanyType = new GraphQLObjectType({
+const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
-    description: {type: GraphQLString}
-  }
+    description: {type: GraphQLString},
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((response) => response.data);
+      }
+    }
+  })
 });
 
-const  UserType = new GraphQLObjectType({
+const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
     age: {type: GraphQLInt},
@@ -30,7 +38,7 @@ const  UserType = new GraphQLObjectType({
           .then((response) => response.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
